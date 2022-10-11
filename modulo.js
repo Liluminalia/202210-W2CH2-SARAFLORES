b009700d6260963d17df61e377f27efe09e42816
Liluminalia <peloazul4@gmail.com>
2022-10-11 22:31:56 +0200

    updating javascript

---
+++ modulo.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+export const strictEquals = (a, b) => {
+    if (isNaN(a) && isNaN(b)) {
+        return false;
+    }
+
+    if (isNaN(a / b)) {
+        return true;
+    }
+
+    let result = Object.is(a, b);
+    let table = [a, b, result];
+    return table;
+};
